4e64ff7206bc5b351942992f88afc2547cc002f2
naveenijeri <naveen.ijeri123@gmail.com>
2020-03-01 19:05:38 +0530

    My Blog App,first commit

---
+++ blog/entries/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
+# Create your models here.
+class Entry(models.Model):
+    entry_title=models.CharField(max_length=50)
+    entry_text=models.TextField()
+    entry_date=models.DateTimeField(auto_now_add=True)
+    entry_author=models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True,on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural="entries"
+
+    def __str__(self):
+        return self.entry_title
+
---
+++ blog/entries/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView
+from .models import Entry
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+class HomeView(LoginRequiredMixin,ListView):
+    model = Entry
+    template_name = 'entries/index.html'
+    context_object_name = 'blog_entries'
+    ordering = ['-entry_date']
+    paginate_by = 3
+
+
+class EntryView(LoginRequiredMixin,DetailView):
+    model=Entry
+    template_name = 'entries/entry_detail.html'
+
+class CreateEntryView(LoginRequiredMixin,CreateView):
+    model=Entry
+    template_name = 'entries/create_entry.html'
+    fields = ['entry_title','entry_text']
+
+    def form_valid(self,form):
+        if self.request.user.is_authenticated:
+               form.instance.entry_author=self.request.user
+               super().form_valid(form)
+               return HttpResponseRedirect(self.get_success_url())
+        else:
+            return self.form_invalid(form)
